332f09c52edf555d586faa223ed5769bb1b7d502
ykenichi <yama.kenichi@yahoo.com>
2017-02-28 21:51:43 -0500

    Updated package.json. Added 404 error checking for routing the show post requests. Included some comments.

--- app/routes.js
+++ app/routes.js
@@ -3,11 +3,12 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-var posts = [];
+// TODO: initialize posts with the data from the file
+var posts = [{"title":"Example title", "thread":"Example text", "created_at":new Date()}];
 
 // create our router object
 var router = express.Router();
-//export our router
+// export our router
 module.exports = router;
 
 // route for our homepage
@@ -16,16 +17,26 @@ router.get('/', function(req, res) {
 	res.render('pages/index', {posts: posts});
 });
 
+// router GET call for the new thread submission  page
 router.get('/new_thread', function (req, res) {
 	console.log('Received a GET request for the new thread page');
 	res.render('pages/newthread');
 });
 
+// router GET call for showing a select post's title and content
 router.get('/show', function(req, res) {
-  selectedPost = posts[req.query.id];
-  res.render('pages/show', {post: selectedPost});
+  var id = req.query.id;
+  // checks to see whether id is a number that is between 0 and the # of posts - 1
+  if(!isNaN(id) && id < posts.length && id >= 0) {
+  	selectedPost = posts[req.query.id];
+  	res.render('pages/show', {post: selectedPost});
+  }
+  else{	// if it is not, returns a 404 not found error
+  	res.send("404 Error, Post Not Found");
+  }
 });
 
+// router POST call for parsing the submitted form and pushing the data into the array of posts
 router.post('/new_thread_post', urlencodedParser, function (req, res) {
 	var d = new Date();
 	posts.push({
@@ -33,5 +44,6 @@ router.post('/new_thread_post', urlencodedParser, function (req, res) {
 		"thread":req.body.thread,
 		"created_at":d
 	});
-	res.render('pages/result')
+	//TODO: Write this new post into a json file containing all of the posts
+	res.render('pages/index', {posts: posts});
 });

dd9d111b5236e998c4728a190bd1ddc6b45b1e4e
heeseungp <heeseung5@gmail.com>
2017-03-01 01:44:19 -0500

    Add comment functionality, need to clean code w. Kenichi

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 // TODO: initialize posts with the data from the file
-var posts = [{"title":"Example title", "thread":"Example text", "created_at":new Date()}];
+var posts = [{"title":"Example title", "thread":"Example text", "created_at":new Date(), "comment":['hi', 'hello']}];
 
 // create our router object
 var router = express.Router();
@@ -25,11 +25,12 @@ router.get('/new_thread', function (req, res) {
 
 // router GET call for showing a select post's title and content
 router.get('/show', function(req, res) {
-  var id = req.query.id;
+  id = req.query.id;
   // checks to see whether id is a number that is between 0 and the # of posts - 1
   if(!isNaN(id) && id < posts.length && id >= 0) {
-  	selectedPost = posts[req.query.id];
-  	res.render('pages/show', {post: selectedPost});
+  	var selectedPost = posts[req.query.id];
+  	var selectedPostComment = selectedPost.comment;
+  	res.render('pages/show', {post: selectedPost, id:id, postcomment:selectedPostComment});
   }
   else{	// if it is not, returns a 404 not found error
   	res.send("404 Error, Post Not Found");
@@ -42,8 +43,19 @@ router.post('/new_thread_post', urlencodedParser, function (req, res) {
 	posts.push({
 		"title":req.body.title,
 		"thread":req.body.thread,
-		"created_at":d
+		"created_at":d,
+		"comment": ['']
 	});
 	//TODO: Write this new post into a json file containing all of the posts
 	res.render('pages/index', {posts: posts});
+});
+
+
+// router POST call for parsing the submitted comment and pushing the data into the array of comments
+router.post('/new_show_post', urlencodedParser, function (req, res) {
+	var selectedPost = posts[id];
+	selectedPost.comment.push(req.body.comment);
+	var selectedPostComment = selectedPost.comment;
+
+	res.render('pages/show', {post: selectedPost, id:id, postcomment:selectedPostComment});
 });

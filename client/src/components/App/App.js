5a4754c7f34c8ed02504b78e1671095cd89c99ed
Daniel Chia <daniel_1118@hotmail.com>
2017-03-02 21:52:26 -0500

    added dummy thread component that must be refactored

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import ThreadContainer from '../Thread/Thread';
 import './App.css';
 
 class App extends Component {
   render() {
+    var threadData = [
+      { title: 'Study guide for interning at the Big 4',
+      post: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.', upvotes: 15 }, { title: 'Study guide for interning at the Big 4',
+      post: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.', upvotes: 15 }, { title: 'Study guide for interning at the Big 4',
+      post: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.', upvotes: 15 }, { title: 'Study guide for interning at the Big 4',
+      post: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.', upvotes: 15 }, { title: 'Study guide for interning at the Big 4',
+      post: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.', upvotes: 15 }
+    ];
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-
-					<h2> Something new is brewing </h2>
-        </p>
+        <ThreadContainer data={threadData} />
       </div>
     );
   }

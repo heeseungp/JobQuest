59df3ab626e3505bb4271bd827515d88444acd95
EAlman21 <enmana21@gmail.com>
2017-03-15 18:51:18 -0400

    still trying to get and post

--- client/src/components/UserInfo/UserInfo.js
+++ client/src/components/UserInfo/UserInfo.js
@@ -7,10 +7,13 @@ import update from 'immutability-helper';
 class UserInfo extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            info:[]
+        };
         this.created_at = '03/14/2017';
         this.company = 'IBM';
-        this.role = 'Software Engineering';
-        this.status = 'Active';
+        this.role = 'Software Engineer';
+        this.status = 'Phone Interview';
     }
     componentDidMount(){
         const url = '/applications/'; 
@@ -18,10 +21,17 @@ class UserInfo extends Component{
         .then(res => {
             console.log('something happening');
             console.log('check me out');
+            //this.created_at = res.data.created_at;
             //this.company = res.data.company;
             //this.role = res.data.role;
             //this.status = res.data.status;
       });
+        const dummy = {company: 'IBM', role: 'Software Engineer', status:'Phone Interview'};
+        axios.post('/applications/', dummy)
+        .then((res) => {console.log('the res is, ', res)})
+            .catch(err => {
+            console.log(err);
+     });
     }
 
     render(){

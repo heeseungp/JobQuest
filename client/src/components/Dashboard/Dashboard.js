76d6b6fa79a775b3656e1e17e7a9a289326c4866
ykenichi <yama.kenichi@yahoo.com>
2017-04-11 03:38:36 -0400

    Added clickable icons to dashboard.

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,6 +1,26 @@
 import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
 import { Card, CardTitle, CardText } from 'material-ui/Card';
+import './Dashboard.css'
 
+const style = {
+  width:200,
+  height:200,
+  padding:50
+};
+
+//Renders a clickable icon that takes the user to another part of the site
+//Uses a bit of CSS magic to make a neat overlay pop up
+let renderIcon = (imagePath, title, link) => {
+  return <Link to={link}>
+    <div className="imagecontainer">
+      <img src={imagePath} style={style} className='image' />
+      <div className="overlay">
+        <div className="overlaytxt">{title}</div>
+      </div>
+    </div>
+  </Link>
+};
 
 const Dashboard = ({ secretData }) => (
   <Card className="container">
@@ -10,6 +30,15 @@ const Dashboard = ({ secretData }) => (
     />
 
     {secretData && <CardText style={{ fontSize: '16px', color: 'green' }}>{secretData}</CardText>}
+    // I LOVE FLEX CONTAINERS!
+    <div className="flexcontainer">
+    {renderIcon('icons/ForumIcon.png','Forums','/forum')}
+    {renderIcon('icons/ApplicationIcon.png','Applications','/app-log')}
+    {renderIcon('icons/InterviewIcon.png','Interview Questions','/interview')}
+    {renderIcon('icons/ProfileIcon.png','Profile','/profile')}
+    {renderIcon('icons/ResourcesIcon.png','Resources','/resources')}
+    </div>
+
   </Card>
 );
 

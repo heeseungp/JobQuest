3327496767cdd29d0a4304c11243067b622fe5a5
ykenichi <yama.kenichi@yahoo.com>
2017-04-13 20:03:43 -0400

    Added navbar. Added a MUI theme to the entire website

--- client/src/components/Base/Base.js
+++ client/src/components/Base/Base.js
@@ -1,35 +1,57 @@
 import React, { PropTypes } from 'react';
-import { Link, IndexLink } from 'react-router';
+import { Link } from 'react-router';
 import Auth from '../../modules/Auth';
+import { AppBar, FlatButton } from 'material-ui';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import muiTheme from '../Theme/Theme';
 
-
-const Base = ({ children }) => (
+// I wanted to seperate the non-authenticaton required tabs from the authentication ones
+// But the stupid parent <div> requirement prevented me from doing that as it messes up the layout
+var renderAllTabs = (
   <div>
-    <div className="top-bar">
-      <div className="top-bar-left">
-        <IndexLink to="/">JobQuest</IndexLink> |
-        <Link to="/forum">Forum</Link> |
-        <Link to="/resources">Resources</Link> |
-        <Link to="/about">About</Link> |
-        <Link to="/interview">Interview Questions</Link>
-      </div>
-
-      {Auth.isUserAuthenticated() ? (
-        <div className="top-bar-right">
-          <Link to="/app-log">App-Log</Link> |
-          <Link to="/profile">Profile</Link> |
-          <Link to="/logout">Log out</Link>
+    {Auth.isUserAuthenticated() ? (
+        <div>
+          <FlatButton label="Home" containerElement={<Link to="/"/>}/> 
+          <FlatButton label="Forum" containerElement={<Link to="/forum"/>}/>
+          <FlatButton label="Resources" containerElement={<Link to="/resources" />}/>
+          <FlatButton label="Interview" containerElement={<Link to="/interview" />}/>
+          <FlatButton label="About" containerElement={<Link to="/about" />}/>
+          <FlatButton label="App Log" containerElement={<Link to="/app-log" />}/>
+          <FlatButton label="Profile" containerElement={<Link to="/profile" />}/>
+          <FlatButton label="Logout" containerElement={<Link to="/logout" />}/>
         </div>
       ) : (
-        <div className="top-bar-right">
-          <Link to="/login">Log in</Link>
-          <Link to="/signup">Sign up</Link>
+        <div>
+           <FlatButton label="Home" containerElement={<Link to="/"/>}/>
+          <FlatButton label="Forum" containerElement={<Link to="/forum"/>}/>
+          <FlatButton label="Resources" containerElement={<Link to="/resources" />}/>
+          <FlatButton label="Interview" containerElement={<Link to="/interview" />}/>
+          <FlatButton label="About" containerElement={<Link to="/about" />}/>
+          <FlatButton label="Log In" containerElement={<Link to="/login" />}/>
+          <FlatButton label="Sign Up" containerElement={<Link to="/signup" />}/>
         </div>
       )}
-    </div>
-
-    {children}
   </div>
+);
+
+const Base = ({ children }) => (
+  
+    <div className="flexnavbar">
+    <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
+      <div>
+        <AppBar
+          title="JobQuest"
+          showMenuIconButton={false}
+          iconElementRight={renderAllTabs}
+          style={{position:"fixed"}}
+        />
+      <div style={{paddingTop:"120"}}>
+      {children}
+      </div>
+    </div>
+    </MuiThemeProvider>
+    </div>
 );
 
 Base.propTypes = {

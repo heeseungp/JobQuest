e73bb9675eb29837a1a512cc1b5ea40490e02cdd
Daniel Chia <daniel_1118@hotmail.com>
2017-04-19 17:23:04 -0400

    improved footer and changed font sizes for feature descriptions

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -9,16 +9,18 @@ import './HomePage.css';
 
 const HomePage = () => (
   <div id="homepage">
+    {/*Big image*/}
     <div className="features">
       <figure className="homepageImg">
         <img src="http://wallresolution.com/wp-content/uploads/2016/09/Fine-Job-Keyboard-Funny-Wallpaper-HD.jpg" />
       </figure>
     </div>
     
+    {/*Forum*/}
     <div className="features">
       <div className="featContent">
         <div className="block feature-desc">
-          <h1>Forum</h1>
+          <h1 className="feat-title">Forum</h1>
           <p>A place where you can ask questions regarding a career in Computer Science</p>
         </div>
         <div className="block">
@@ -29,6 +31,7 @@ const HomePage = () => (
       </div>
     </div>
     
+    {/*Application history*/}
     <div className="features">
       <div className="featContent">
         <div className="block">
@@ -37,17 +40,18 @@ const HomePage = () => (
           </figure>
         </div>
         <div className="block feature-desc">
-          <h1>Forum</h1>
-          <p>A place where you can ask questions regarding a career in Computer Science</p>
+          <h1 className="feat-title">Application History</h1>
+          <p>Keep a detailed log of all your submitted applications</p>
         </div>
       </div>
     </div>
 
+    {/*Interview Questions*/}
     <div className="features">
       <div className="featContent">
         <div className="block feature-desc">
-          <h1>Forum</h1>
-          <p>A place where you can ask questions regarding a career in Computer Science</p>
+          <h1 className="feat-title">Interview Questions</h1>
+          <p>Check our list of popular interview questions</p>
         </div>
         <div className="block">
           <figure className="homepageImg">
@@ -57,6 +61,7 @@ const HomePage = () => (
       </div>
     </div>
     
+    {/*Resources*/}
     <div className="features">
       <div className="featContent">
         <div className="block">
@@ -65,8 +70,8 @@ const HomePage = () => (
           </figure>
         </div>
         <div className="block feature-desc">
-          <h1>Forum</h1>
-          <p>A place where you can ask questions regarding a career in Computer Science</p>
+          <h1 className="feat-title">Resources</h1>
+          <p>Check our list of resources to aid you in your job search</p>
         </div>
       </div>
     </div>
